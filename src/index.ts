a520782ae2a824f09a55124f28fe27a32c2f7289
Koushik Dey <koushik.dey@live.com>
2023-03-07 01:35:48 +0200

    basic express and ts setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello TS!");
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+  <div>
+  <h1>Here I go...</h1>
+  </div>
+  `);
+});
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
