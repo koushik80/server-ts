90bd42eb27ef886e5e81ff7e71f35af46edad638
Koushik Dey <koushik.dey@live.com>
2023-03-07 02:24:54 +0200

    express router and parsing from bodies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-  <div>
-  <h1>Here I go...</h1>
-  </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3001, () => {
   console.log('Listening on port 3001');
