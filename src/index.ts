c9f6de552958eb9797117ebd2a4a5ceaf6ff7f65
Koushik Dey <koushik.dey@live.com>
2023-03-07 17:46:46 +0200

    refactor with decorator

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginControllers';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['abcdgh'] }))
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3001, () => {
   console.log('Listening on port 3001');
