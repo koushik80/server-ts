8414722c8cb028d3c5f0fea5f30b056d7efdee6b
Koushik Dey <koushik.dey@live.com>
2023-03-07 21:19:58 +0200

    body validators

--- src/controllers/LoginControllers.ts
+++ src/controllers/LoginControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { get, controller } from './decorators';
+import { get, post, controller, bodyValidator } from './decorators';
 
 @controller('/auth')
 class LoginControllers {
@@ -18,5 +18,18 @@ class LoginControllers {
      <button>Submit</button>
    </form>
   `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password')
+    }
   }
 }
